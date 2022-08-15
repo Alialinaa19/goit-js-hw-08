c2d85d7ccf92aa6dee2551622e6a8fefc72bfb1a
Alina Guscha <alialinaa19@gmail.com>
2022-08-15 22:46:42 +0300

    1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,7 +1,7 @@
 
 import Player from '@vimeo/player';
 import { throttle } from 'lodash';
-const iframe = document.querySelector('.vimeo-player');
+const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
 player.on('timeupdate',  throttle( e => {
