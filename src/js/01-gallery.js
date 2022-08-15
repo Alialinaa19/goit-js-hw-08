633635273de30136dafe839a22af4b3cd08c86d7
Alina Guscha <alialinaa19@gmail.com>
2022-08-15 20:00:43 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,15 +1,14 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-
-import SimpleLightbox from 'simplelightbox';
+// @ts-ignore
+import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
 
-
 function createLi(array) {
-    return array.reduce((acc, item) => acc + `<div class="gallery__item"><a class="gallery__link" href="${item.original}"><img src="${item.preview}" data-source= "${item.original}" alt = "${item.description}" class="gallery__image"/></a></div>`, "")
+    return array.reduce((acc, item) => acc + `<a class="gallery__item" href="${item.original}"><img title="${item.description}" src="${item.preview}" alt = "${item.description}" class="gallery__image"/></a>`, "")
 }
 
 const result = createLi(galleryItems);
@@ -18,26 +17,11 @@ const list = document.querySelector(".gallery");
 list.insertAdjacentHTML("beforeend", result);
 console.log(list)
 
-function onImgClick(e) {
-    e.preventDefault();
 
-    if (e.target.nodeName !== 'IMG') {
-		return
-	}
-    console.log(e.target.classList.contains("gallery__image"))
-    
-    const selectedImage = e.target.getAttribute('data-source')
-
-    const instance = basicLightbox.create(`
-    <img src="${selectedImage}" width="800" height="600">
-`)
-
-    instance.show()
-    list.addEventListener('keydown', e => {
-		if (e.key === 'Escape') {
-			instance.close()
-		}
-	})
-}
-
-list.addEventListener("click", onImgClick)
+// const lightbox = new SimpleLightbox('.gallery a');
+// new SimpleLightbox('.gallery a', {captionDelay: 250});
+// gallery.on('show.simplelightbox', function () {
+// });
+new SimpleLightbox('.gallery a', {
+    captionDelay: 250
+})
