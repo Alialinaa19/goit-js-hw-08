fb65bde2f1074b6e8b2ec957f99d31ec96c9874a
Alina Guscha <alialinaa19@gmail.com>
2022-08-15 22:02:52 +0300

    1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,8 @@
 
+import Vimeo from "@vimeo/player";
+const iframe = document.querySelector('.vimeo-player');
+const player = new Vimeo.Player(iframe);
+
+player.on('play', function() {
+    console.log('played the video!');
+});
